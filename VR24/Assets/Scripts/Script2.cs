0155acd7fed4d1a025a5fda30bfc7035256951cf
kolxxx00 <rv683235@stcain18.cn>
2024-12-17 03:02:52 +0300

    Задание №2
    Collision

---
+++ VR24/Assets/Scripts/Script2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script2 : MonoBehaviour
+{
+    public int a = 10;
+    public Text Mytxt;
+    public int z, y, c;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        a--;
+        Mytxt.text = a.ToString();
+    }
+}
+    
